192573e4f815c4efd3b2a476b87af82ea2da7790
Omi Harjani <omiharjani@gmail.com>
2017-10-30 15:35:24 +0530

    datepicker

--- Dev/project_one/assignment/UserDatabase/UserInfo/models.py
+++ Dev/project_one/assignment/UserDatabase/UserInfo/models.py
@@ -7,7 +7,6 @@ class user_info(models.Model):
 	first_name = models.CharField(max_length=200)
 	last_name = models.CharField(max_length=200)
 	email = models.EmailField()
-	# mobile = models.IntegerField()
 	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
 	mobile = models.CharField(validators=[phone_regex], max_length=15, blank=True) # validators should be a list
 	dob = models.DateField()
